42cb46ff6dc53a248f5ac665b4bebd253d44fd69
S.P. Vishwakarma <itspratap255@gmail.com>
2026-05-27 08:50:37 +0530

    project security

--- smartcalc/settings.py
+++ smartcalc/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+secret_key = os.environ.get('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4fg#ar_qxmgpd6g@k45x%7%r=an04gl+(m*90vq!j%-)9!uksq'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'Pritam96Anaconda.pythonanywhere.com' ]
 
 
 # Application definition
